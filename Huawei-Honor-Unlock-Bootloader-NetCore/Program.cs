c68ad73e1054934b63d8977ec472dd6db78cc396
Cinorid <cinorid@gmail.com>
2020-12-04 12:09:35 +0330

    Add project files.

---
+++ Huawei-Honor-Unlock-Bootloader-NetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Huawei_Honor_Unlock_Bootloader_NetCore
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ Huawei-Honor-Unlock-Bootloader-NetFramework/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Huawei_Honor_Unlock_Bootloader_NetFramework
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
